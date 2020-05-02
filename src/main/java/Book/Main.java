bcaf3960312ea2334eb31995ff5dd657393d7fde
ahmedelahwany <61867082+ahmedelahwany@users.noreply.github.com>
2020-05-02 20:10:03 +0200

    update Main.java (3rd)

--- src/main/java/Book/Main.java
+++ src/main/java/Book/Main.java
@@ -15,8 +15,8 @@ public class Main {
         {
             BookDao bookDao = handle.attach((BookDao.class));
             bookDao.createTable();
-            bookDao.insert(new Book("9780785834588", "Dante Aligieri","The Divine Comedy", Book.Format.Hardback, "Book Sales Inc",LocalDate.parse("2016-12-22"), 736, true));
-            bookDao.insert(new Book("9781409181637", "Alex Michaelides","The Silent Patient : The Richard and Judy bookclub pick and Sunday Times Bestseller", Book.Format.Paperback, "Orion Publishing Co",LocalDate.parse("2020-1-15"), 352, true));
+            bookDao.insert(new Book("9780785834588", "Dante Aligieri","The Divine Comedy", Book.Format.HARDBACK, "Book Sales Inc",LocalDate.parse("2016-12-22"), 736, true));
+            bookDao.insert(new Book("9781409181637", "Alex Michaelides","The Silent Patient : The Richard and Judy bookclub pick and Sunday Times Bestseller", Book.Format.PAPERBACK, "Orion Publishing Co",LocalDate.parse("2020-1-15"), 352, true));
 
 
 
